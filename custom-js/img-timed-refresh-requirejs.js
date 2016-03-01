7b664fc09ccda4f113fe97afd15364bfb8faf1f2
agrassi <a.grassi@sintraconsulting.eu>
2016-03-01 15:29:26 +0100

    fix img timed refresh

--- custom-js/img-timed-refresh-requirejs.js
+++ custom-js/img-timed-refresh-requirejs.js
@@ -1,30 +1,32 @@
 require(['jquery'],function(jq){
 	function updateRefreshableImages()
 	{
-		jq('.img-timed-refresh').each(function(){		
-			var $img=jq(this);
-			var imgSrc=$img.attr("src");
-			var originalSrc=$img.data("src");
-			var newSrc=imgSrc;
-			var dateStr="dtUpdate="+new Date().getTime();
-			if (originalSrc==null || originalSrc==undefined)
-			{
-				$img.data("src",imgSrc);
-			}
-			if (imgSrc.indexOf("dtUpdate")>0){
-				newSrc=imgSrc.replace(/dtUpdate=\d*/,dateStr);
-			}else{
-				if (imgSrc.indexOf("?")>0)
+		if (jq('.img-timed-refresh').size()>0){
+			jq('.img-timed-refresh').each(function(){		
+				var $img=jq(this);
+				var imgSrc=$img.attr("src");
+				var originalSrc=$img.data("src");
+				var newSrc=imgSrc;
+				var dateStr="dtUpdate="+new Date().getTime();
+				if (originalSrc==null || originalSrc==undefined)
 				{
-					newSrc=imgSrc+"&"+dateStr;
+					$img.data("src",imgSrc);
 				}
-				else{
-					newSrc=imgSrc+"?"+dateStr;
+				if (imgSrc.indexOf("dtUpdate")>0){
+					newSrc=imgSrc.replace(/dtUpdate=\d*/,dateStr);
+				}else{
+					if (imgSrc.indexOf("?")>0)
+					{
+						newSrc=imgSrc+"&"+dateStr;
+					}
+					else{
+						newSrc=imgSrc+"?"+dateStr;
+					}
 				}
-			}
-			$img.attr("src",newSrc);
-		});
-		setTimeout(updateRefreshableImages,5000);
+				$img.attr("src",newSrc);
+			});
+			setTimeout(updateRefreshableImages,5000);
+		}
 	}
 	updateRefreshableImages();
 });

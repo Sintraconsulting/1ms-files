6ca5e4362c5fb73b6dadc86af82843bf515afe70
Federico Stendardi <f.stendardi@sintraconsulting.eu>
2016-03-07 15:04:39 +0100

    fixed custom js to conform to async require load

--- custom-js/img-timed-refresh-requirejs.js
+++ custom-js/img-timed-refresh-requirejs.js
@@ -1,32 +1,42 @@
-require(['jquery'],function(jq){
-	function updateRefreshableImages()
-	{
-		if (jq('.img-timed-refresh').size()>0){
-			jq('.img-timed-refresh').each(function(){		
-				var $img=jq(this);
-				var imgSrc=$img.attr("src");
-				var originalSrc=$img.data("src");
-				var newSrc=imgSrc;
-				var dateStr="dtUpdate="+new Date().getTime();
-				if (originalSrc==null || originalSrc==undefined)
-				{
-					$img.data("src",imgSrc);
-				}
-				if (imgSrc.indexOf("dtUpdate")>0){
-					newSrc=imgSrc.replace(/dtUpdate=\d*/,dateStr);
-				}else{
-					if (imgSrc.indexOf("?")>0)
-					{
-						newSrc=imgSrc+"&"+dateStr;
-					}
-					else{
-						newSrc=imgSrc+"?"+dateStr;
-					}
-				}
-				$img.attr("src",newSrc);
-			});
-			setTimeout(updateRefreshableImages,5000);
-		}
-	}
-	updateRefreshableImages();
-});
+(function () {
+  if (typeof omsOnInit === 'function') {
+    omsOnInit(doWork);
+  } else {
+    doWork();
+  }
+
+  function doWork() {
+    require(['jquery'], function (jq) {
+      function updateRefreshableImages() {
+        if (jq('.img-timed-refresh').size() > 0) {
+          jq('.img-timed-refresh').each(function () {
+            var $img = jq(this);
+            var imgSrc = $img.attr("src");
+            var originalSrc = $img.data("src");
+            var newSrc = imgSrc;
+            var dateStr = "dtUpdate=" + new Date().getTime();
+            if (originalSrc == null || originalSrc == undefined) {
+              $img.data("src", imgSrc);
+            }
+            if (imgSrc.indexOf("dtUpdate") > 0) {
+              newSrc = imgSrc.replace(/dtUpdate=\d*/, dateStr);
+            } else {
+              if (imgSrc.indexOf("?") > 0) {
+                newSrc = imgSrc + "&" + dateStr;
+              }
+              else {
+                newSrc = imgSrc + "?" + dateStr;
+              }
+            }
+            $img.attr("src", newSrc);
+          });
+          setTimeout(updateRefreshableImages, 5000);
+        }
+      }
+      updateRefreshableImages();
+    });
+  }
+})();
+
+
+
--- custom-js/multi_level_menu.js
+++ custom-js/multi_level_menu.js
@@ -1,33 +1,46 @@
-$.getJSON("/menu.json", function( data ) {
-    var checkExist = setInterval(function() {
-        if ($('#main-menu-top').length) {
-            clearInterval(checkExist);
-            var pages = data.languages[siteLanguage.urlKey];
-            for (i in pages) {
-                var page = pages[i];
-                if (page.children!=null && page.children!=undefined)
-                {
-                    $navLi = $("#nav-li-page-"+page.id);
-                    $navHref = $("#nav-a-page-"+page.id);
-                    //disable main link
-                    $navHref.attr("href","#");
+(function () {
+  if (typeof omsOnInit === 'function') {
+    omsOnInit(function () {
+      require(['jquery', 'oms/context'], function ($, Context) {
+        doWork($, Context.siteLanguage);
+      });
+    });
+  } else {
+    doWork($, siteLanguage);
+  }
 
-                    $navLi.addClass("dropdown");
-                    $ul=$("<ul/>",{"class":"dropdown-menu","role":"menu"});
-                    for (childIdx in page.children)
-                    {
-                        var child = page.children[childIdx];
-                        $ul.append(
-                            $("<li/>",{"id":"nav-li-page-"+child.id})
-                                .append(
-                                $("<a/>",{"href":child.url,"id":"nav-a-page-"+child.id}).append(
-                                    child.title
-                                )
-                            ))
-                    }
-                    $navLi.append($ul);
-                }
+  function doWork($, siteLanguage) {
+    $.getJSON("/menu.json", function (data) {
+      var checkExist = setInterval(function () {
+        if ($('#main-menu-top').length) {
+          clearInterval(checkExist);
+          var pages = data.languages[siteLanguage.urlKey];
+          for (var i in pages) {
+            var page = pages[i];
+            if (page.children != null && page.children != undefined) {
+              var $navLi = $("#nav-li-page-" + page.id);
+              var $navHref = $("#nav-a-page-" + page.id);
+              //disable main link
+              $navHref.attr("href", "#");
+
+              $navLi.addClass("dropdown");
+              var $ul = $("<ul/>", { "class": "dropdown-menu", "role": "menu" });
+              for (var childIdx in page.children) {
+                var child = page.children[childIdx];
+                $ul.append(
+                  $("<li/>", { "id": "nav-li-page-" + child.id })
+                    .append(
+                      $("<a/>", { "href": child.url, "id": "nav-a-page-" + child.id }).append(
+                        child.title
+                        )
+                      ))
+              }
+              $navLi.append($ul);
             }
+          }
         }
-    }, 50);
-});
+      }, 50);
+    });
+  }
+})();
+
